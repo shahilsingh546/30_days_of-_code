13a1caca6372e23839dd1265d315df687397993e
shahilsingh546 <shahilsingh456@gmail.com>
2024-07-22 23:11:51 +0530

    added day15

---
+++ DSA/Day-15/Kadane'sAlgo.cpp
@@ -0,0 +1,17 @@
+//  Maximum Subarray
+
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+         int res = nums[0];
+        int maxsum =nums[0];
+
+        for(int i =1;i<nums.size();i++)
+        {
+            maxsum = max(maxsum+nums[i],nums[i]);
+            res = max (res,maxsum);
+        }
+        return res;
+    }
+};
